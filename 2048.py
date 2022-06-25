dc5929d92afc89cc7748fcb00b8735edc4ef7b95
Константин <MindlinKS@yandex.ru>
2022-06-25 21:58:39 +0300

    refactor def display_output

--- 2048.py
+++ 2048.py
@@ -31,38 +31,12 @@ class MirrorWindow(QtWidgets.QWidget):
                     1024: 'background-color: FireBrick',
                     2048: 'background-color: DarkRed'}
 
-        self.ui.le00.setText(str(x.field[0][0]))
-        self.ui.le00.setStyleSheet(color_le.get(x.field[0][0]))
-        self.ui.le01.setText(str(x.field[0][1]))
-        self.ui.le01.setStyleSheet(color_le.get(x.field[0][1]))
-        self.ui.le02.setText(str(x.field[0][2]))
-        self.ui.le02.setStyleSheet(color_le.get(x.field[0][2]))
-        self.ui.le03.setText(str(x.field[0][3]))
-        self.ui.le03.setStyleSheet(color_le.get(x.field[0][3]))
-        self.ui.le10.setText(str(x.field[1][0]))
-        self.ui.le10.setStyleSheet(color_le.get(x.field[1][0]))
-        self.ui.le11.setText(str(x.field[1][1]))
-        self.ui.le11.setStyleSheet(color_le.get(x.field[1][1]))
-        self.ui.le12.setText(str(x.field[1][2]))
-        self.ui.le12.setStyleSheet(color_le.get(x.field[1][2]))
-        self.ui.le13.setText(str(x.field[1][3]))
-        self.ui.le13.setStyleSheet(color_le.get(x.field[1][3]))
-        self.ui.le20.setText(str(x.field[2][0]))
-        self.ui.le20.setStyleSheet(color_le.get(x.field[2][0]))
-        self.ui.le21.setText(str(x.field[2][1]))
-        self.ui.le21.setStyleSheet(color_le.get(x.field[2][1]))
-        self.ui.le22.setText(str(x.field[2][2]))
-        self.ui.le22.setStyleSheet(color_le.get(x.field[2][2]))
-        self.ui.le23.setText(str(x.field[2][3]))
-        self.ui.le23.setStyleSheet(color_le.get(x.field[2][3]))
-        self.ui.le30.setText(str(x.field[3][0]))
-        self.ui.le30.setStyleSheet(color_le.get(x.field[3][0]))
-        self.ui.le31.setText(str(x.field[3][1]))
-        self.ui.le31.setStyleSheet(color_le.get(x.field[3][1]))
-        self.ui.le32.setText(str(x.field[3][2]))
-        self.ui.le32.setStyleSheet(color_le.get(x.field[3][2]))
-        self.ui.le33.setText(str(x.field[3][3]))
-        self.ui.le33.setStyleSheet(color_le.get(x.field[3][3]))
+        for index, horizontal_layout in enumerate(self.ui.verticalLayout.children()):
+            for index_ in range(horizontal_layout.count()):
+                value_for_set = x.field[index][index_]
+                item = horizontal_layout.itemAt(index_)
+                item.widget().setText(str(value_for_set))
+                item.widget().setStyleSheet(color_le.get(value_for_set))
 
     @QtCore.Slot()
     def help(self):
